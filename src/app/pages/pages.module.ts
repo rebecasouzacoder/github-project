698b0fa72dc42fafd6f9d4a1981c50cd160e6dae
Rebeca Souza <rebeca.souza@guide.com.br>
2023-06-15 08:42:58 -0300

    feat: criar component de profile

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,12 +3,18 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { SharedModule } from '../shared/shared.module';
+import { CardSearchComponent } from './home/components/card-search/card-search.component';
+import { CardUsersComponent } from './home/components/card-users/card-users.component';
+import { PaginatorComponent } from './home/components/paginator/paginator.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    CardSearchComponent,
+    CardUsersComponent,
+    PaginatorComponent,
   ],
   imports: [
     SharedModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,19 +3,13 @@ import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
-import { CardSearchComponent } from './components/card-search/card-search.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { CardUsersComponent } from './components/card-users/card-users.component';
-import { PaginatorComponent } from './components/paginator/paginator.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
-    CardSearchComponent,
-    CardUsersComponent,
-    PaginatorComponent,
     LoadingComponent
   ],
   imports: [
@@ -33,9 +27,6 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    CardSearchComponent,
-    CardUsersComponent,
-    PaginatorComponent,
     CommonModule,
     LoadingComponent,
     MatProgressSpinnerModule
